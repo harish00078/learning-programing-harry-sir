3c4eb1f07165ef01ca0d6fc7bd54345d191b6d70
harish kumar <harishpathania150@gmail.com>
2022-04-28 21:59:58 +0530

    upgrade

--- chapter7/chapter7_practice_set/01_pr_practice_set.py
+++ chapter7/chapter7_practice_set/01_pr_practice_set.py
@@ -1,3 +1,4 @@
+# this whole program is used to make a multiplication system.we can multiply any number
 num = int(input("enter the number :"))
 for i in range(1,11):
     print(str(num) + "x" + str(i) + "=" + str(i*num))

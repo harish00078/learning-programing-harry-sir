2b71669be5c93e997f92642916d8c31586fa658b
harish kumar <harishpathania150@gmail.com>
2022-05-18 20:51:33 +0530

    upgrade

--- chapter10/practice_set_chapter10/02_pr_set.py
+++ chapter10/practice_set_chapter10/02_pr_set.py
@@ -1,1 +1,20 @@
-# 2question:
+# 2question: write a class  calculator capable of finding square,cube,and square root of a number.
+
+class calculator:
+    def __init__(self,num):
+        self.number = num
+    
+    def square(self):
+        print(f"the value of {self.number} square is {self.number**2}")
+    
+    def squareroot(self):
+        print(f"the value of {self.number} square is {self.number**0.5}")
+    
+    def cube(self):
+        print(f"the value of {self.number} square is {self.number**3}")
+    
+    
+a = calculator(9)
+a.square()
+a.squareroot()
+a.cube()
